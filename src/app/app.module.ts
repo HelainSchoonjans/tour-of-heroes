0199b7db45cb0bd8a145e1f886b525f22f322348
Helain Schoonjans <helain90@hotmail.com>
2017-07-21 18:42:41 +0200

    Finished the toh-pt6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-details.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroService } from './hero.service';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,13 +19,16 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent,
     DashboardComponent,
+    HeroSearchComponent,
     HeroesComponent,
     HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [
     HeroService
